9dcd8ff556079f2b8aef4ef5b37ec814fabe741b
Ayan Khan <aw1800606@gmail.com>
2026-06-02 18:33:15 +0500

    Two ways binding

--- Todo web app/src/App.jsx
+++ Todo web app/src/App.jsx
@@ -1,11 +1,22 @@
-import Todo from "./Components/Todo";
-
+import { useState } from "react"
 const App = () => {
+  const [title, setTitle] = useState("")
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Form Submitted by" , title );
+    setTitle("")
+  }
   return (
     <>
-      
       <div className="todo">
-          <Todo />
+        <form onSubmit={(e) => {
+          handleSubmit(e)
+        }}>
+          Enter your Name <br /> <input value={title} onChange={(e) => {
+            setTitle(e.target.value)
+          }} type="text" /> <br />
+          <button>Submit</button>
+        </form>
       </div>
 
     </>
